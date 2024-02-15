6d33f9db63b915ac611f4e4cdecc8760d19774ba
Илья Тупицин <ilhatupitsin@yandex.ru>
2024-02-15 10:03:55 +0300

    Добавьте файлы проекта.

---
+++ фибоначи/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace фибоначи
+{
+    class Fibonacci<T>
+    {
+        public static List<T> GenerateFibonacci(int n)
+        {
+            List<T> fibonacci = new List<T>();
+            dynamic x = 0;
+            dynamic y = 1;
+            fibonacci.Add((T)x);
+            for (int i = 2; i <= n; i++)
+            {
+                fibonacci.Add((T)y);
+                dynamic temp = y;
+                y += x;
+                x = temp;
+            }
+            return fibonacci;
+        }
+    }
+}
---
+++ фибоначи/Program.cs
@@ -0,0 +1,59 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество чисел Фибоначчи:");
+        int n = int.Parse(Console.ReadLine());
+        List<int> fibonacci = фибоначи.Fibonacci<int>.GenerateFibonacci(n);
+        foreach (var i in fibonacci)
+        {
+            Console.WriteLine(i + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        Console.WriteLine("F1) 1");
+        Console.WriteLine("F2) 1");
+
+        int n_1 = 1;
+        int n_2 = 1;
+
+        static dynamic fiba<T>(T n)
+        {
+        return fiba(n);
+        }
+        for (int i = 3; i <= n; ++i)
+        {
+            var n_3 = n_1 + n_2;
+            Console.WriteLine("F{0}) {1}", i, n_3);
+            n_1 = n_2;
+            n_2 = n_3;
+        }
+        Console.ReadKey();
+    }
+}
+*/
